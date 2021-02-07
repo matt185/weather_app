e9f9128d3506c268d4a7f81c7e34ccfa80869b2a
Mattia <59755795+matt185@users.noreply.github.com>
2021-02-07 15:26:46 +0900

    setup default layout

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center"></div>
+      <v-card>
+        <v-card-title class="headline">Weather App</v-card-title>
+        <v-card-text>
+          <p>Simple weather app that permit to check the weather condition by typing the city name</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
